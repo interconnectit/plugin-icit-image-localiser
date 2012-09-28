0888602e7545dfed9d0c01400fcea175fa9f3354
rob <rob@705268d9-db5d-0410-b4eb-c557ab941a74>
2012-09-28 09:15:16 +0000

    updated - fixed some issues with menu page generation
    git-svn-id: https://office.interconnectit.com/svn/InterconnectIT/plugins/icit-image-localiser@6549 705268d9-db5d-0410-b4eb-c557ab941a74

--- icit-plugin/icit-plugin.php
+++ icit-plugin/icit-plugin.php
@@ -45,9 +45,6 @@ if ( ! class_exists( 'icit_plugins' ) ) {
 			// create plugin page
 			add_action( 'admin_menu', array( $this, 'plugin_pages' ), 100 );
 
-			// icit plugin page CSS
-			add_action( 'admin_print_styles', array( $this, 'css' ) );
-
 			// handle save after pages are prepped and settings registered
 			add_action( 'admin_init', array( $this, 'save' ), 100 );
 
@@ -83,7 +80,6 @@ if ( ! class_exists( 'icit_plugins' ) ) {
 
 			// add plugin to list
 			$this->plugins[ $id ] = $args;
-
 		}
 
 
@@ -99,6 +95,13 @@ if ( ! class_exists( 'icit_plugins' ) ) {
 				// choose the current page if any
 				if ( isset( $_REQUEST[ 'page' ] ) && $_REQUEST[ 'page' ] == $plugin[ 'menu_slug' ] )
 					$this->plugin = $id;
+
+				// plugin page CSS & JS
+				if ( $this->is_plugin_page() ) {
+					add_action( 'admin_print_styles', array( $this, 'plugin_css' ) );
+					add_action( 'admin_enqueue_styles', array( $this, 'css' ) );
+					add_action( 'admin_enqueue_scripts', array( $this, 'js' ) );
+				}
 
 				// create page
 				if ( $plugin[ 'parent_slug' ] )
@@ -124,7 +127,7 @@ if ( ! class_exists( 'icit_plugins' ) ) {
 				wp_die( __( 'You do not have permission to use this page.' ) );
 
 			echo '
-		<div class="wrap icit-plugin">';
+		<div class="wrap icit-plugin metabox-holder">';
 
 			// title
 			if ( isset( $plugin[ 'icon' ] ) )
@@ -185,17 +188,24 @@ if ( ! class_exists( 'icit_plugins' ) ) {
 					echo '<table class="form-table">' . $settings_fields . '</table>';
 				if ( ! empty( $settings_sections ) )
 					echo $settings_sections;
-				submit_button();
 
 				echo ob_get_clean();
 
 			}
 
+			ob_start();
 			// normal context metaboxes
 			do_meta_boxes( $id, 'normal', $plugin );
 
 			// advanced context metaboxes
 			do_meta_boxes( $id, 'advanced', $plugin );
+
+			$metaboxes = ob_get_clean();
+
+			echo $metaboxes;
+
+			if ( ! empty( $settings_fields ) || ! empty( $settings_sections ) || ! empty( $meta_boxes ) )
+				submit_button();
 
 			echo '
 					</div>
@@ -213,10 +223,9 @@ if ( ! class_exists( 'icit_plugins' ) ) {
 			do_action( "icit_plugin_save", $this->plugin );
 		}
 
-		function css() {
+		function plugin_css() {
 			?>
 			<style>
-				.icit-plugin {  }
 				.icit-plugin .right-column { float: right; width: 280px; }
 				.icit-plugin .left-column { float: left; width: 100%; margin-right: -300px; }
 				.icit-plugin .left-column .column-inner { margin-right: 300px; }
@@ -227,8 +236,23 @@ if ( ! class_exists( 'icit_plugins' ) ) {
 				.icit-plugin .icit-branding p cite { display: none; }
 				.icit-plugin .icit-branding div { margin: 10px; }
 				.icit-plugin .icit-branding .version { margin: 5px 10px 10px; font-size: 16px; color: #787878; }
+				.icit-plugin .meta-box-sortables { clear: both; margin-top: 20px; }
 			</style>
 			<?php
+		}
+
+		function css() {
+			wp_enqueue_style( 'postbox' );
+			do_action( "enqueue_styles_{$this->plugin}" );
+		}
+
+		function js() {
+			wp_enqueue_script( 'postbox' );
+			do_action( "enqueue_scripts_{$this->plugin}" );
+		}
+
+		function is_plugin_page() {
+			return ( isset( $_GET[ 'page' ] ) && $_GET[ 'page' ] == $this->plugin ) || ( isset( $_GET[ 'option_page' ] ) && $_GET[ 'option_page' ] == $this->plugin );
 		}
 
 		/**
